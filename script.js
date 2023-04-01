20d26a7ea18918a615f8b84f1ec5bca041df4af3
ali <adadashipour@gmail.com>
2023-04-01 20:08:19 +0100

    Create addBookToLibrary(), adds a new book to the mylibrary[] array.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const mylibrary = [];
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -10,5 +12,20 @@ function Book(title, author, pages, read) {
     }
 }
 
-const HPDH = new Book("Harry Potter and the Deathly Hallows", "J. K. Rowling", 607, false);
-console.log(HPDH.info()); // Output: "Harry Potter and the Deathly Hallows by J. K. Rowling, 607 pages, not read yet"
+const HPDH = new Book("Harry Potter and the Deathly Hallows", "J. K. Rowling", 607, true);
+const Spare = new Book("Spare", "Prince Harry", 416, false);
+const ItStartsWithUs = new Book("It Starts with Us", "Colleen Hoover", 336, false);
+// console.log(HPDH.info()); // Output: "Harry Potter and the Deathly Hallows by J. K. Rowling, 607 pages, not read yet"
+
+
+function addBookToLibrary(){
+    const title = prompt("Enter book title:");
+    const author = prompt("Enter book  author:")
+    const pages = prompt("Enter number of pages:");
+    const read = confirm("Have you read this book?");
+
+    const newBook = new Book(title, author, pages, read);
+    mylibrary.push(newBook)
+}
+
+addBookToLibrary()
