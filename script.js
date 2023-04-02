5989a8c70a4e0f206e51bb295712743093b8bde5
ali <adadashipour@gmail.com>
2023-04-02 20:08:56 +0100

    Pushed the template objects to mylibrary[]. displayBook() iterates through the array and displays each book as a table.

--- script.js
+++ script.js
@@ -12,11 +12,12 @@ function Book(title, author, pages, read) {
     }
 }
 
-const HPDH = new Book("Harry Potter and the Deathly Hallows", "J. K. Rowling", 607, true);
-const Spare = new Book("Spare", "Prince Harry", 416, false);
-const ItStartsWithUs = new Book("It Starts with Us", "Colleen Hoover", 336, false);
+const tempbook1 = new Book("Harry Potter and the Deathly Hallows", "J. K. Rowling", 607, true);
+const tempbook2 = new Book("Spare", "Prince Harry", 416, false);
+// const tempbook3 = new Book("It Starts with Us", "Colleen Hoover", 336, false);
 // console.log(HPDH.info()); // Output: "Harry Potter and the Deathly Hallows by J. K. Rowling, 607 pages, not read yet"
-
+mylibrary.push(tempbook1);
+mylibrary.push(tempbook2);
 
 function addBookToLibrary(){
     const title = prompt("Enter book title:");
@@ -28,4 +29,39 @@ function addBookToLibrary(){
     mylibrary.push(newBook)
 }
 
-addBookToLibrary()
+function displayBooks() {
+    const table = document.createElement("table");
+    table.innerHTML = `
+      <tr>
+        <th>Title</th>
+        <th>Author</th>
+        <th>Pages</th>
+        <th>Read</th>
+      </tr>
+    `;
+
+    for (let i = 0; i < mylibrary.length; i++) {
+        const book = mylibrary[i];
+        const row = table.insertRow();
+    
+        const titleCell = row.insertCell();
+        titleCell.textContent = book.title;
+    
+        const authorCell = row.insertCell();
+        authorCell.textContent = book.author;
+    
+        const pagesCell = row.insertCell();
+        pagesCell.textContent = book.pages;
+    
+        const readCell = row.insertCell();
+        readCell.textContent = book.read ? "Read" : "Not read yet";
+      }
+    
+      document.body.appendChild(table);
+    }
+    
+const addBookBtn = document.getElementById("add-book-btn");
+addBookBtn.addEventListener("click", () => {
+    addBookToLibrary();
+    displayBooks();
+});
